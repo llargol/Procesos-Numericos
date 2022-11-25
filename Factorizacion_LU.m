181eb25bf55a7dcd0da2dab83f8d809acb01e808
Leonardo Largo <110748874+llargol@users.noreply.github.com>
2022-11-25 08:56:55 -0500

    Add files via upload

---
+++ Factorizacion_LU.m
@@ -0,0 +1,32 @@
+%LU: Calcula la solución de un sistema de ecuaciones Ax=b 
+% mediante la factorización de A=LU ya sea sin pivoteo piv=0 
+% o usando pivoteo parcial piv=1. Donde A es de tamaño nxn y b de tamaño nx1
+
+function [x, L, U] = LU(A,b,n,Piv)
+
+A=input('Ingrese la matriz A de tamaño nxn');
+b=input('Ingrese la matriz independiente b de tamaño nx1');
+n=input('Ingrese el tamano de la matriz');
+Piv=input('Ingrese 0 para calcular la solucion sin pivoteo y 1 usando pivoteo parcial:');
+    P=eye(n);
+    L=P;
+    for k=1:n-1
+        if Piv==1
+            [A, P]=pivLU(A,P,n,k);
+        end
+        for i=k+1:n
+            M=A(i,k)/A(k,k);
+            for j=k:n
+                A(i,j)=A(i,j)-M*A(k,j);
+            end
+            A(i,k)=M;
+        end
+    end
+    U=triu(A);
+    L=L+tril(A,-1);
+    B=P*b;
+    LB=[L,B];
+    z=sustpro(LB,n);
+    Uz=[U,z];
+    x=sustreg(Uz,n);
+end
